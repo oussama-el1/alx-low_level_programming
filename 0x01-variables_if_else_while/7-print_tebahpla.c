83cffd8c14c745e6ad70bd6c8edf98cc3ee41847
oussama-el1 <elhadraoui284@gmail.com>
2023-07-14 09:09:16 -0700

    inverse

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main -task inverse
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char c = 'z';
+
+while (c >= 'a')
+{
+putchar(c);
+c--;
+}
+return (0);
+}
