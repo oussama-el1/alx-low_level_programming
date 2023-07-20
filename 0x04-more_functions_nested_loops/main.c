663e3628f749dad4b80d27ef15d8c0eb4ed9f332
oussama-el1 <elhadraoui284@gmail.com>
2023-07-20 13:40:08 -0700

    solve !

---
+++ 0x04-more_functions_nested_loops/main.c
@@ -0,0 +1,37 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+void print_diagonal(int n)
+{
+int i,j;
+
+if (n <= 0)
+{
+putchar('\n');
+}
+else
+for (i = 0;i < n;i++)
+{
+for (j = 0;j <= i;j++)
+{
+if (j == i)
+putchar('\\');
+else
+putchar(' ');
+}
+putchar('\n');
+}
+}
+
+int main(void)
+{
+    print_diagonal(0);
+    print_diagonal(2);
+    print_diagonal(10);
+    print_diagonal(-4);
+    return (0);
+}
