df5dfbf218a841bf3667131b9ee4c89ce55ab939
oussama-el1 <elhadraoui284@gmail.com>
2023-07-24 07:41:45 -0700

    task 6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * puts2 - prints one char out of 2 of a string
+ * followed by a new line
+ * @str: string to print the chars from
+ */
+void puts2(char *str)
+{int l;
+
+l = 0;
+while (str[l] != '\0')
+{
+l++;
+}
+for (i = 0; i < l; i++)
+{
+if (i % 2 == 0)
+_putchar(str[i]);
+}
+_putchar('\n');
+}
