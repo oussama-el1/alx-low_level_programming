65bd440d046f6f306df60b767736fd10b5aaf2c3
oussama-el1 <elhadraoui284@gmail.com>
2023-08-06 13:11:53 -0700

    task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - prints name
+ * @argc: counter
+ * @argv: pointer to an pointer
+ * Return: Always 0 (Success)
+ */
+
+int main(int argc, char **argv)
+{
+printf("%s\n", *argv);
+return (0);
+}
