b577a24047419a20ab3e93c9d28b588bbd2e12e4
oussama-el1 <elhadraoui284@gmail.com>
2023-08-06 14:28:10 -0700

    task 3

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -2,16 +2,21 @@
 #include <stdlib.h>
 
 /**
- * main: multiplucation
+ * main - multiplucation
  * @argc: count
  * @argv: arrays
- * Return: Always 1 (Success)
+ * Return: 0 (Success), 1 (Error)
  */
 int main(int argc, char *argv[])
 {
 if (argc == 3)
+{
 printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+return (0);
+}
 else
+{
 printf("Error\n");
 return (1);
+}
 }
