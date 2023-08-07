4fb06cb22fbf0ceef726ccd27020fddb2b8e4b74
oussama-el1 <elhadraoui284@gmail.com>
2023-08-07 06:30:48 -0700

    task 3

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * **alloc_grid - two dem array
+ * @width: w
+ * @height: h
+ * Return: ponter to pointer in (succes), NULL in (Error)
+ */
+int **alloc_grid(int width, int height)
+{
+int **ptr;
+int i, j;
+if (width <= 0 || height <= 0)
+return (NULL);
+
+ptr = (int **) malloc(sizeof(int *) * height);
+if (ptr == NULL)
+{
+return (NULL);
+}
+for (i = 0; i < height; i++)
+{
+ptr[i] = (int *) malloc(sizeof(int) * width);
+}
+for (i = 0; i < height; i++)
+{
+for (j = 0; j < width; j++)
+{
+ptr[i][j] = 0;
+}
+}
+return (ptr);
+}
