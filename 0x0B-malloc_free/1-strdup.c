8be4d47cb273dfbe00c38ba98059037be4467db9
oussama-el1 <elhadraoui284@gmail.com>
2023-08-07 03:56:35 -0700

    task 1

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * *_strdup - function for duplicate;
+ * @str: string to duplcate
+ * Return: always ponter
+ */
+char *_strdup(char *str)
+{
+char *c;
+int i = 0, l = 0;
+
+if (str == NULL)
+return (NULL);
+while (str[i] != '\0')
+{
+l++;
+i++;
+}
+c = (char *) malloc(sizeof(char) * l);
+if (c == NULL)
+return (NULL);
+i = 0;
+while (i <= l)
+{
+*(c + i) = str[i];
+i++;
+}
+return (c);
+}
