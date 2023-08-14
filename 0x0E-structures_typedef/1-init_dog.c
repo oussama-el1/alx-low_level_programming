a5864da97d1a3c3b514f07a1623de2f1a12ea1c0
oussama-el1 <elhadraoui284@gmail.com>
2023-08-14 08:05:52 -0700

    task 1

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -14,6 +14,6 @@ void init_dog(struct dog *d, char *name, float age, char *owner)
 if (d == NULL)
 	d = malloc(sizeof(struct dog));
 d->name = name;
-d->owner = ownwe;
+d->owner = owner;
 d->age = age;
 }
