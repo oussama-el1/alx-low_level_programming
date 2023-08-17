a76fa80eae4c9d6815f6d406064d3e400d4f8573
oussama-el1 <elhadraoui284@gmail.com>
2023-08-17 14:01:04 -0700

    task 0

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * print_name - function to print name
+ * @f: function pointer
+ * @name: name printed
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+*f(name);
+}
---
+++ 0x0F-function_pointers/main.h
@@ -0,0 +1,9 @@
+#ifndef MIAN_H
+#define MAIN_H
+
+void print_name(char *name, void (*f)(char *));
+
+
+
+
+#endif
