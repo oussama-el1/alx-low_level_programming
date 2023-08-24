d9f287b8e5fefcff3095c336ddc76ffd98ec6cb9
oussama-el1 <elhadraoui284@gmail.com>
2023-08-24 16:26:32 -0700

    0-print_list.c

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,30 @@
+#include "lists.h"
+
+/**
+ * print_list - print all elements list
+ * @h: ponter to header
+ * Return: unsigned int of elt list 
+ */
+
+size_t print_list(const list_t *h)
+{
+unsigned int i = 0;
+const list_t *cor;
+if (h == NULL)
+	{
+		printf("vide list");
+		exit(1);
+	}
+cor = h;
+	while(cor != NULL)
+	{
+		if (cor->str == NULL)
+		printf("[0] (nil)\n");
+		else
+		printf("[%d] %s\n",cor->len, cor->str);
+
+		i++;
+		cor = cor->next;
+	}
+return (i);
+}
