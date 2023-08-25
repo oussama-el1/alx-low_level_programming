670d361e40bca4d14dac02671c222eba8b0e3ef8
oussama-el1 <elhadraoui284@gmail.com>
2023-08-25 09:24:08 -0700

    100-first.c

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#include "lists.h"
+
+void print_line(void) __attribute__ ((constructor));
+
+/**
+ * print_line - print
+ */
+
+void print_line(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
