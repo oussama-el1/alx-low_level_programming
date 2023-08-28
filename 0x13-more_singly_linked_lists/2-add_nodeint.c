3b5260fd00686bc64d5d30e32086b09ac406d5bf
oussama-el1 <elhadraoui284@gmail.com>
2023-08-28 13:24:55 -0700

    2-add_nodeint.c

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -2,8 +2,9 @@
 
 /**
  * add_nodeint - add node to list
- * 
- *
+ * @head: ponter to head
+ * @n: int tom assign
+ * Return: addres of new elt
  */
 
 listint_t *add_nodeint(listint_t **head, const int n)
