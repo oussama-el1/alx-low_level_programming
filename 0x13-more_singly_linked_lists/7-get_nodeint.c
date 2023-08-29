1b4c76bdd826f078abdb45b0ec0b5c94307cf014
oussama-el1 <elhadraoui284@gmail.com>
2023-08-29 13:02:47 -0700

    7-get_nodeint.c

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,34 @@
+#include "lists.h"
+
+/**
+ * get_nodeint_at_index - function
+ * @head: head ponter
+ * @index: inedex de element
+ */
+
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+listint_t *cour = NULL;
+listint_t *temp = NULL;
+unsigned int i = 0;
+
+temp = head;
+while(temp->next != NULL)
+{
+i++;
+temp = temp->next;
+}
+
+if (head == NULL || index > i)
+{
+return (NULL);
+}
+
+cour = head;
+i = 0;
+	for (i = 0; i < index; i++)
+	{
+		cour=cour->next;
+	}
+return (cour);
+}
