3f52a1e1b01878be55a244e4106f9c7f212df88d
oussama-el1 <elhadraoui284@gmail.com>
2024-02-07 14:40:10 +0100

    task 0 to linux

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,22 @@
+#include <search_algos.h>
+
+/**
+ * linear_search - linrear search function
+ * @array: array pointer to first elt
+ * @size: size of array
+ * @value: elt to sherche
+ */
+
+int linear_search(int *array, size_t size, int value)
+{
+    size_t i = 0;
+
+    for (i = 0; i < size; i++)
+    {
+        if (array[i] == value)
+        {
+            return (i);
+        }
+    }
+    return (-1);
+}
