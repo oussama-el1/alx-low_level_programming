3b5a793002145ec9f047088ed478a7673f2ceaf5
oussama-el1 <elhadraoui284@gmail.com>
2024-02-07 06:02:42 -0800

    task 0

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -1,22 +1,24 @@
-#include <search_algos.h>
+#include "search_algos.h"
 
 /**
  * linear_search - linrear search function
  * @array: array pointer to first elt
  * @size: size of array
  * @value: elt to sherche
- */
+ * Return: integer the index or -1
+*/
 
 int linear_search(int *array, size_t size, int value)
 {
-    size_t i = 0;
+size_t i = 0;
 
-    for (i = 0; i < size; i++)
-    {
-        if (array[i] == value)
-        {
-            return (i);
-        }
-    }
-    return (-1);
+for (i = 0; i < size; i++)
+{
+printf("Value checked array[%ld] = %d\n", i, array[i]);
+if (array[i] == value)
+{
+return (i);
+}
+}
+return (-1);
 }
--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -2,6 +2,8 @@
 #define SEARCH_ALGOS
 
 #include <stdlib.h>
+#include <stdio.h>
 int linear_search(int *array, size_t size, int value);
 
 #endif
+
