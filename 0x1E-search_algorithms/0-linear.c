71ee2c0d87d71ed74e7396101f7d7044bbd2d6f6
oussama-el1 <elhadraoui284@gmail.com>
2024-02-07 06:10:46 -0800

    task 0

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -12,6 +12,11 @@ int linear_search(int *array, size_t size, int value)
 {
 int i = 0;
 
+if (array == NULL)
+{
+return (-1);
+}
+
 for (i = 0; i < (int)size; i++)
 {
 printf("Value checked array[%u] = %d\n", i, array[i]);
